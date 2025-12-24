553f45e8d0b3a0330e2dc0e288b862052caebb1a
raselahmed0295 <rasel.ahmed0295@gmail.com>
2025-12-25 01:13:11 +0600

    Fix Header filename casing

--- src/utility/Root.jsx
+++ src/utility/Root.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { Outlet } from "react-router";
 
-const Root = () => {
-  return (
-    <div>
-      <Header></Header>
-      <Outlet></Outlet>
-      <Footer></Footer>
-    </div>
-  );
-};
+const Root = () => (
+  <div>
+    <Header />
+    <Outlet />
+    <Footer />
+  </div>
+);
 
 export default Root;
